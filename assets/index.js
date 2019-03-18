c5f09eab4778849f3a6bf9686073c277f4d59b50
Kizito Akhilome <kizito@akhilo.me>
2019-03-18 23:12:00 +0100

    switch over to making an AJAX call to API (#3)
    - add spinner
    - show spinner based on whether the user is currently hitting the api

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,5 @@
+const API = 'http://localhost:5500/api/v1';
+
 // Helper Function
 const getValue = id => document.getElementById(id).value;
 
@@ -37,34 +39,56 @@ const specs = {
   headphone,
   fingerprint,
   extras
-}
+};
 
 // Summary
 const summary = {
   intro,
   software,
   hardware
-}
+};
 
 // Listen for input changes and update specs object accordingly
-document.querySelectorAll('input').forEach(e => e.addEventListener('keyup', function() {
-  for (const key in specs) {
-    specs[key] = getValue(`${key}`);
-  }
-}));
+document.querySelectorAll('input').forEach(e =>
+  e.addEventListener('keyup', function() {
+    for (const key in specs) {
+      specs[key] = getValue(`${key}`);
+    }
+  })
+);
 
-document.querySelectorAll('textarea').forEach(e => e.addEventListener('keyup', function() {
-  for (const key in summary) {
-    summary[key] = getValue(`${key}`);
-  }
-}));
+document.querySelectorAll('textarea').forEach(e =>
+  e.addEventListener('keyup', function() {
+    for (const key in summary) {
+      summary[key] = getValue(`${key}`);
+    }
+  })
+);
+
+const toggleLoadingState = () =>
+  document.querySelector('.loader-container').classList.toggle('show');
 
 // Get Download button
 const done = document.querySelector('#done');
 
-// Prep download button everytime a key is hit
-document.querySelector('html').addEventListener('keyup', function() {
-  const data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({specs, summary}));
-  done.href = `data:${data}`;
-  done.download = `${specs.device}.json`;
-});
+// Hit API
+const saveArticle = () => {
+  fetch(`${API}/articles`, {
+    method: 'POST',
+    body: JSON.stringify({ title: specs.device, specs, summary }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(res => {
+      toggleLoadingState();
+      return res.json();
+    })
+    .then(response => {
+      setTimeout(() => {
+        toggleLoadingState();
+        console.log('🔥', response);
+      }, 3000);
+    })
+    .catch(e => console.error(e));
+};
+
+done.onclick = saveArticle;
